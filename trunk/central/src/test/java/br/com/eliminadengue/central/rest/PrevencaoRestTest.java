037be7c00660832d1e6dd34b609c540d900628c6
guilherme.alves.vargas@gmail.com <guilherme.alves.vargas@gmail.com@89fcfcea-5411-fe45-df15-1ac782aa8dde>
2015-02-11 20:26:12 +0000

    primeiro commit

---
+++ trunk/central/src/test/java/br/com/eliminadengue/central/rest/PrevencaoRestTest.java
@@ -0,0 +1,65 @@
+package br.com.eliminadengue.central.rest;
+
+import br.com.eliminadengue.central.model.Prevencao;
+import java.net.URI;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class PrevencaoRestTest {
+
+    private static HttpServer server;
+    private static WebTarget target;
+    public static final URI BASE_URI = UriBuilder.fromUri("http://localhost")
+            .port(7575)
+            .build();
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        ResourceConfig rc = new ResourceConfig(PrevencaoRest.class);
+        server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+
+        server.start();
+        Client client = ClientBuilder.newClient();
+        target = client.target(BASE_URI);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+        server.shutdown();
+    }
+
+    @Test
+    public void getIt() {
+        Prevencao prevencao =  (Prevencao) target.path("/prevencao")
+                .queryParam("nome", "ralos")
+                .request()
+                .get(Object.class);
+
+        assertTrue(prevencao != null);
+    }
+}
