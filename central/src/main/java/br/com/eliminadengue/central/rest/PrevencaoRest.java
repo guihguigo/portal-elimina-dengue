d67712b2b033eaef51fd99dc7a162065df26950f
guilherme.alves.vargas <guilherme.alves.vargas@gmail.com>
2015-02-18 00:01:28 +0000

    Testando CRUID restfull

--- central/src/main/java/br/com/eliminadengue/central/rest/PrevencaoRest.java
+++ central/src/main/java/br/com/eliminadengue/central/rest/PrevencaoRest.java
@@ -8,6 +8,8 @@ package br.com.eliminadengue.central.rest;
 import br.com.eliminadengue.central.model.Prevencao;
 import br.com.eliminadengue.central.perssistence.PrevencaoDao;
 import br.com.eliminadengue.central.perssistence.Perssiste;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -15,8 +17,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,13 +37,27 @@ public class PrevencaoRest {
     private PrevencaoDao prevencaoDao;
     
     @GET
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    public Response encontrar(@QueryParam("codigoCelular") int codigoCelular, 
-            @QueryParam("codigoFoco") int codigoFoco) {
+    @Path("{codigoCelular}/{codigoFoco}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response encontrar(@PathParam("codigoCelular") Integer codigoCelular, 
+            @PathParam("codigoFoco") Integer codigoFoco) {
         prevencaoDao.encontrar(codigoCelular, codigoFoco);
        
         return Response.ok(new Prevencao()).build();
+    }
+    
+    @GET
+    @Produces("application/json")
+    public List<Prevencao> todos() {
+        prevencaoDao.todos();
+        ArrayList<Prevencao> prevencoes = new ArrayList<>();
+        prevencoes.add(new Prevencao());
+        prevencoes.add(new Prevencao());
+        prevencoes.add(new Prevencao());
+        prevencoes.add(new Prevencao());
+        
+        return prevencoes;
     }
     
     @POST
