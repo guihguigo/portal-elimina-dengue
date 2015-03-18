22668e0fa51ce6b287bc1e277d19cbdbf4bdbb15
guilherme.alves.vargas@gmail.com <guilherme.alves.vargas@gmail.com@89fcfcea-5411-fe45-df15-1ac782aa8dde>
2015-03-18 04:18:14 +0000

    modificações no design

--- trunk/central/src/main/java/br/com/eliminadengue/central/model/PercentualPrevencoesPorMes.java
+++ trunk/central/src/main/java/br/com/eliminadengue/central/model/PercentualPrevencoesPorMes.java
@@ -5,47 +5,22 @@
  */
 package br.com.eliminadengue.central.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author Guilherme Alves
  */
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
-public class PercentualPrevencoesPorMes extends PercentualPrevencoes {
-    private double percentualAtrasada;
-    private double percentualEfetuada;
-
-    public void setPercentualAtrasada(double percentAtualrasada) {
-        this.percentualAtrasada = percentAtualrasada;
-    }
-
-    public void setPercentualEfetuada(double percentualEfetuada) {
-        this.percentualEfetuada = percentualEfetuada;
-    }
-
-    public double getPercentualAtrasada() {
-        return percentualAtrasada;
-    }
-
-    public double getPercentualEfetuada() {
-        return percentualEfetuada;
-    }
-    private int mes;
-    private String nomeMes;
-
-    public String getNomeMes() {
-        String[] meses = {"JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
+@XmlRootElement(name = "prevencoes")
+public class PercentualPrevencoesPorMes implements PercentualPrevencoes {
+    public final String[] MESES = {"JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
             "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"};
-        return meses[mes];
-    }
-
-    public void setNomeMes(String nomeMes) {
-        this.nomeMes = nomeMes;
-    }
+    
+    private double percentualEfetuadas;
+    private double percentualAtrasada;
+    private int mes;
+    private String nomeFoco;
+    private String nomeMes;
 
     public void setMes(int mes) {
         this.mes = mes;
@@ -59,6 +34,46 @@ public class PercentualPrevencoesPorMes extends PercentualPrevencoes {
     public String toString() {
         return "Percentual de prevenções por mês";
     }
-    
-    
+
+    @Override
+    public void setPercentualAtrasada(double percentualAtrasada) {
+        this.percentualAtrasada = percentualAtrasada;
+    }
+
+    @Override
+    public void setPercentualEfetuada(double percentualEfetuada) {
+        this.percentualEfetuadas = percentualEfetuada;
+    }
+
+    @Override
+    public double getPercentualAtrasada() {
+        return this.percentualAtrasada;
+    }
+
+    @Override
+    public double getPercentualEfetuada() {
+        return this.percentualEfetuadas;
+    }
+
+    @Override
+    public String getNomeMes() {
+        return MESES[mes];
+    }
+
+    @Override
+    public void setNomeMes(String nomeMes) {
+        this.nomeMes = nomeMes;
+
+    }
+
+    @Override
+    public String getNomeFoco() {
+        return this.nomeFoco;
+    }
+
+    @Override
+    public void setNomeFoco(String nomeFoco) {
+        this.nomeFoco = nomeFoco;
+    }
+
 }
